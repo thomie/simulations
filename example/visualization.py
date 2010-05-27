7746ef7f90de3ab7fe923800e17257dba201ef8c
thomie <miedema@amolf.nl>
2010-05-27 14:26:20 +0200

    Add example/visualization.py.

---
+++ example/visualization.py
@@ -0,0 +1,5 @@
+base_directory = '/storage1/miedema/code/'
+paraview_scripts_directory = base_directory + 'egfrd/visualization/'
+simulation_data_directory = base_directory + 'simulations/example/data/run/'
+
+execfile(paraview_scripts_directory + 'pipeline.py')
