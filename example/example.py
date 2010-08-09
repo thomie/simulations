04f1c0d453fac377be241c4df8c88c36559e940a
thomie <miedema@amolf.nl>
2010-08-09 18:49:21 +0200

    Brackets can be omitted.

--- example/example.py
+++ example/example.py
@@ -174,39 +174,39 @@ if EGFRD_MODEL == True:
 
     '''
     if REACTIONS and WORLD:
-        m.add_reaction_rule([A],    [B],    kf_1)
-        m.add_reaction_rule([B],    [A],    kb_1)
-        m.add_reaction_rule([A, B], [C],    kf_2)
-        m.add_reaction_rule([C],    [A, B], kb_2)
+        m.add_reaction_rule(A,      B,      kf_1)
+        m.add_reaction_rule(B,      A,      kb_1)
+        m.add_reaction_rule([A, B], C,      kf_2)
+        m.add_reaction_rule(C,      [A, B], kb_2)
         if DECAY:
-            m.add_reaction_rule([C], [], kf_1)
+            m.add_reaction_rule(C, [], kf_1)
 
     if REACTIONS and BOX:
-        m.add_reaction_rule([(A, b)],         [(B, b)],         kf_1)
-        m.add_reaction_rule([(B, b)],         [(A, b)],         kb_1)
-        m.add_reaction_rule([(A, b), (B, b)], [(C, b)],         kf_2)
-        m.add_reaction_rule([(C, b)],         [(A, b), (B, b)], kb_2)
+        m.add_reaction_rule((A, b),           (B, b),           kf_1)
+        m.add_reaction_rule((B, b),           (A, b),           kb_1)
+        m.add_reaction_rule([(A, b), (B, b)], (C, b),           kf_2)
+        m.add_reaction_rule((C, b),           [(A, b), (B, b)], kb_2)
         if DECAY:
             m.add_reaction_rule([(C, b)], [], kf_1)
 
     if REACTIONS and MEMBRANE1:
-        m.add_reaction_rule([(A, m1)],          [(B, m1)],          kf_1)
-        m.add_reaction_rule([(B, m1)],          [(A, m1)],          kb_1)
-        m.add_reaction_rule([(A, m1), (B, m1)], [(C, m1)],          kf_2)
-        m.add_reaction_rule([(C, m1)],          [(A, m1), (B, m1)], kb_2)
+        m.add_reaction_rule((A, m1),            (B, m1),            kf_1)
+        m.add_reaction_rule((B, m1),            (A, m1),            kb_1)
+        m.add_reaction_rule([(A, m1), (B, m1)], (C, m1),            kf_2)
+        m.add_reaction_rule((C, m1),            [(A, m1), (B, m1)], kb_2)
         if DECAY:
-            m.add_reaction_rule([(C, m1)], [], kf_1)
+            m.add_reaction_rule((C, m1), [], kf_1)
 
     if REACTIONS and MEMBRANE2:
         pass
 
     if REACTIONS and DNA:
-        m.add_reaction_rule([(A, d)],         [(B, d)],         kf_1)
-        m.add_reaction_rule([(B, d)],         [(A, d)],         kb_1)
-        m.add_reaction_rule([(A, d), (B, d)], [(C, d)],         kf_2)
-        m.add_reaction_rule([(C, d)],         [(A, d), (B, d)], kb_2)
+        m.add_reaction_rule((A, d),           (B, d),           kf_1)
+        m.add_reaction_rule((B, d),           (A, d),           kb_1)
+        m.add_reaction_rule([(A, d), (B, d)], (C, d),           kf_2)
+        m.add_reaction_rule((C, d),           [(A, d), (B, d)], kb_2)
         if DECAY:
-            m.add_reaction_rule([(C, d)], [], kf_1)
+            m.add_reaction_rule((C, d), [], kf_1)
 
 
     '''Add surface binding reaction rules.
@@ -215,18 +215,18 @@ if EGFRD_MODEL == True:
     if SURFACE_BINDING_REACTIONS and MEMBRANE1 and BOX:
         # ParticleType C can bind from the box to membrane1. The 
         # membrane is reflective, by default, to ParticleTypes A and B.
-        m.add_reaction_rule([(C, b)], [(C, m1)], kon)
+        m.add_reaction_rule((C, b), (C, m1), kon)
 
     if SURFACE_BINDING_REACTIONS and MEMBRANE2 and BOX:
         # Membrane 2 absorbs all particles.
-        m.add_reaction_rule([(A, b)], [(0, m2)], kon)
-        m.add_reaction_rule([(B, b)], [(0, m2)], kon)
-        m.add_reaction_rule([(C, b)], [(0, m2)], kon)
+        m.add_reaction_rule((A, b), (0, m2), kon)
+        m.add_reaction_rule((B, b), (0, m2), kon)
+        m.add_reaction_rule((C, b), (0, m2), kon)
 
     if SURFACE_BINDING_REACTIONS and DNA and BOX:
         # ParticleType C can bind from the box to the dna. The dna is 
         # reflective, by default, to ParticleTypes A and B.
-        m.add_reaction_rule([(C, b)], [(C, d)], kon)
+        m.add_reaction_rule((C, b), (C, d), kon)
 
 
     ''' Add surface unbinding reaction rules.
@@ -234,14 +234,14 @@ if EGFRD_MODEL == True:
     '''
     if SURFACE_UNBINDING_REACTIONS and MEMBRANE1 and BOX:
         # Species C can unbind from membrane1 to the box.
-        m.add_reaction_rule([(C, m1)], [(C, b)], koff)
+        m.add_reaction_rule((C, m1), (C, b), koff)
 
     if SURFACE_UNBINDING_REACTIONS and MEMBRANE2 and BOX:
         pass
 
     if SURFACE_UNBINDING_REACTIONS and DNA and BOX:
         # Species C can unbind from the dna to the box.
-        m.add_reaction_rule([(C, d)], [(C, b)], koff)
+        m.add_reaction_rule((C, d), (C, b), koff)
 
 
 else:
