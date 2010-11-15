2116e1b189fdfca905174eae264789d10607dd3f
thomie <miedema@amolf.nl>
2010-11-15 14:06:12 +0100

    visualization is now a Python module

--- example/paraview.py
+++ example/paraview.py
@@ -1,5 +1,18 @@
-base_directory = '/home/miedema/code/'
-paraview_scripts_directory = base_directory + 'egfrd/visualization/'
-simulation_data_directory = base_directory + 'simulations/example/data/run/'
 
-execfile(paraview_scripts_directory + 'pipeline.py')
+base_directory = '/home/miedema/code/amolf/'
+egfrd_directory = base_directory + 'egfrd/'
+
+#simulation_data_directory = base_directory + 'simulations/example/planar-surface_6-particles_200-steps/'
+#simulation_data_directory = base_directory + 'simulations/example/12-particles_200-steps/'
+simulation_data_directory = base_directory + 'simulations/example/data/run'
+
+
+import sys
+sys.path.append(egfrd_directory)
+
+from visualization import pipeline
+
+
+# Create pipeline.
+p = pipeline.Pipeline(egfrd_directory, simulation_data_directory)
+
