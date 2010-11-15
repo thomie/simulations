9c315e7003c7c036aaa6a60248308c3dbc74ef20
thomie <miedema@amolf.nl>
2010-11-15 18:31:36 +0100

    Updata paraview.py for bd_vs_gfrd

--- mapk/bd-vs-gfrd/paraview.py
+++ mapk/bd-vs-gfrd/paraview.py
@@ -1,6 +1,15 @@
 base_directory = '/home/miedema/code/amolf/'
-paraview_scripts_directory = base_directory + 'egfrd/visualization/'
-#simulation_data_directory = base_directory + 'simulations/bd-vs-gfrd/data/bd-1800_cpu_s-1.0_kk_vs_p/'
-simulation_data_directory = base_directory + 'simulations/bd-vs-gfrd/data/gfrd-1800_cpu_s-1.0_kk_vs_p/'
+egfrd_directory = base_directory + 'egfrd/'
 
-execfile(paraview_scripts_directory + 'pipeline.py')
+#simulation_data_directory = base_directory + 'simulations/mapk/bd-vs-gfrd/data/bd-1800_cpu_s-1.0_kk_vs_p/'
+simulation_data_directory = base_directory + 'simulations/mapk/bd-vs-gfrd/data/gfrd-1800_cpu_s-1.0_kk_vs_p/'
+
+import sys
+sys.path.append(egfrd_directory)
+
+from visualization import pipeline
+
+
+# Create pipeline.
+p = pipeline.Pipeline(egfrd_directory, simulation_data_directory)
+
