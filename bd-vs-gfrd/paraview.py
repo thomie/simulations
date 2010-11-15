8d37ce8f3fb8f493516a30afadc9388bf5e0d12b
thomie <miedema@amolf.nl>
2010-11-15 13:59:41 +0100

    modified:   bd-vs-gfrd/paraview.py

--- bd-vs-gfrd/paraview.py
+++ bd-vs-gfrd/paraview.py
@@ -1,5 +1,6 @@
-base_directory = '/storage1/miedema/code/'
+base_directory = '/home/miedema/code/amolf/'
 paraview_scripts_directory = base_directory + 'egfrd/visualization/'
+#simulation_data_directory = base_directory + 'simulations/bd-vs-gfrd/data/bd-1800_cpu_s-1.0_kk_vs_p/'
 simulation_data_directory = base_directory + 'simulations/bd-vs-gfrd/data/gfrd-1800_cpu_s-1.0_kk_vs_p/'
 
 execfile(paraview_scripts_directory + 'pipeline.py')
