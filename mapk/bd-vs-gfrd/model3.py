1a795284b0309f0f64fe8a8704ec420e5b95c4f2
thomie <miedema@amolf.nl>
2010-11-15 18:31:58 +0100

    Update model3.py for bd_vs_gfrd

--- mapk/bd-vs-gfrd/model3.py
+++ mapk/bd-vs-gfrd/model3.py
@@ -4,7 +4,8 @@
 # python -O model3.py bd
 
 from logger import *
-from vtklogger import *
+from visualization import vtklogger
+from model import *
 import sys
 import time
 
@@ -13,8 +14,8 @@ import math
 
 
 LOGGER = False
-INTERVAL = 1 #6
-CPU_SECONDS = 10 #1800
+INTERVAL = 6
+CPU_SECONDS = 1800
 
 V = 1e-15
 D_ratio = 1
@@ -28,25 +29,23 @@ simulator = sys.argv[1]
 
 
 # V in liter, L in meter
-L = math.pow( V * 1e-3, 1.0 / 3.0 )
+L = math.pow(V * 1e-3, 1.0 / 3.0)
 N = 180
-matrixSize = min( max( 3, int( (3 * N) ** (1.0/3.0) ) ), 60 )
+matrixSize = min(max(3, int((3 * N) ** (1.0/3.0))), 60)
 #print 'matrixSize=', matrixSize
 
 
 if simulator == 'gfrd':
     from egfrd import *
-    w = World(L, matrixSize)
-    s = EGFRDSimulator(w)
     dataDirectory = 'gfrd-'
-    no_shells = False
+    show_shells = False
 elif simulator == 'bd':
     from bd import *
     w = World(L, matrixSize)
     s = BDSimulator(w)
     #s.core.dtFactor = DEFAULT_DT_FACTOR
     dataDirectory = 'bd-'
-    no_shells = True
+    show_shells = False
 
 dataDirectory += str(CPU_SECONDS) + '_cpu_s-' + str(KK_VS_P) + '_kk_vs_p'
 
@@ -55,9 +54,9 @@ dataDirectory += str(CPU_SECONDS) + '_cpu_s-' + str(KK_VS_P) + '_kk_vs_p'
 model='mapk3'
 
 if ti == 0:
-    ki = float( 'inf' )
+    ki = float('inf')
 else:
-    ki = math.log( 2 ) / ti
+    ki = math.log(2) / ti
 
 D_ref = 1e-12
 
@@ -68,25 +67,35 @@ if D_mode == 'normal':
 elif D_mode == 'fixed':
     D_react = D_ref
 
-box1 = CuboidalRegion( [0,0,0],[L,L,L] )
-
 radius = 2.5e-9
 
-m = ParticleModel()
+m = ParticleModel(L)
 
-K = m.new_species_type( 'K', D_move, radius )
-KK = m.new_species_type( 'KK', D_move, radius )
-P = m.new_species_type( 'P', D_move, radius )
-Kp = m.new_species_type( 'Kp', D_move, radius )
-Kpp = m.new_species_type( 'Kpp', D_move, radius )
-K_KK = m.new_species_type( 'K_KK', D_move, radius )
-Kp_KK = m.new_species_type( 'Kp_KK', D_move, radius )
-Kpp_P = m.new_species_type( 'Kpp_P', D_move, radius )
-Kp_P = m.new_species_type( 'Kp_P', D_move, radius )
+K = Species('K', D_move, radius)
+KK = Species('KK', D_move, radius)
+P = Species('P', D_move, radius)
+Kp = Species('Kp', D_move, radius)
+Kpp = Species('Kpp', D_move, radius)
+K_KK = Species('K_KK', D_move, radius)
+Kp_KK = Species('Kp_KK', D_move, radius)
+Kpp_P = Species('Kpp_P', D_move, radius)
+Kp_P = Species('Kp_P', D_move, radius)
 
 # inactive forms
-KKi = m.new_species_type( 'KKi', D_move, radius )
-Pi = m.new_species_type( 'Pi', D_move, radius )
+KKi = Species('KKi', D_move, radius)
+Pi = Species('Pi', D_move, radius)
+
+m.add_species_type(K)
+m.add_species_type(KK)
+m.add_species_type(P)
+m.add_species_type(Kp)
+m.add_species_type(Kpp)
+m.add_species_type(K_KK)
+m.add_species_type(Kp_KK)
+m.add_species_type(Kpp_P)
+m.add_species_type(Kp_P)
+m.add_species_type(KKi)
+m.add_species_type(Pi)
 
 color_dict = {
               2:1, # enzyme KK
@@ -104,7 +113,12 @@ color_dict = {
              }
 
 
-s.setModel(m)
+if simulator == 'gfrd':
+    w = create_world(m, matrixSize)
+    s = EGFRDSimulator(w)
+elif simulator == 'bd':
+    w = create_world(m, matrixSize)
+    s = BDSimulator(w)
 
 #  1 2   K + KK   <-> K_KK
 #  3     K_KK       -> Kp + KKi
@@ -119,18 +133,18 @@ s.setModel(m)
 
 
 sigma = radius * 2
-kD = k_D( D_react * 2, sigma )
+kD = k_D(D_react * 2, sigma)
 
-N_K = C2N( 200e-9, V ) 
-N_KK = KK_VS_P * 2 * C2N( 50e-9, V )
-N_P = (1 - KK_VS_P) * 2 * C2N( 50e-9, V )
+N_K = C2N(200e-9, V) 
+N_KK = KK_VS_P * 2 * C2N(50e-9, V)
+N_P = (1 - KK_VS_P) * 2 * C2N(50e-9, V)
 
 
-s.throwInParticles( K, N_K, box1 )
+throw_in_particles(s.world, K, N_K)
 print 'Number of K =', N_K
-s.throwInParticles( KK, N_KK, box1 )
+throw_in_particles(s.world, KK, N_KK)
 print 'Number of KK =', N_KK
-s.throwInParticles( P, N_P, box1 )
+throw_in_particles(s.world, P, N_P)
 print 'Number of P =', N_P
 
 if simulator == 'gfrd':
@@ -138,70 +152,70 @@ if simulator == 'gfrd':
     endTime = 0
     while 1:
         s.step()
-        nextTime = s.scheduler.getTopTime()
+        nextTime = s.scheduler.top[1].time
         if nextTime > endTime:
-            s.stop( endTime )
+            s.stop(endTime)
             break
 s.reset()
 
-k1 = k_a( Mtom3( 0.02e9 ), kD )
-k2 = k_d( 1.0, Mtom3( 0.02e9 ), kD )
+k1 = k_a(per_M_to_m3(0.02e9), kD)
+k2 = k_d(1.0, per_M_to_m3(0.02e9), kD)
 k3 = 1.5
-k4 = k_a( Mtom3( 0.032e9 ), kD )
-k5 = k_d( 1.0, Mtom3( 0.032e9 ), kD )
+k4 = k_a(per_M_to_m3(0.032e9), kD)
+k5 = k_d(1.0, per_M_to_m3(0.032e9), kD)
 k6 = 15.0
 
-r1 = createBindingReactionRule( K, KK, K_KK, k1 )
-m.network_rules.add_reaction_rule( r1 )
-r2 = createUnbindingReactionRule( K_KK, K, KK, k2 )
-m.network_rules.add_reaction_rule( r2 )
-r3 = createUnbindingReactionRule( K_KK, Kp, KKi, k3 )
-m.network_rules.add_reaction_rule( r3 )
+r1 = create_binding_reaction_rule(K, KK, K_KK, k1)
+m.network_rules.add_reaction_rule(r1)
+r2 = create_unbinding_reaction_rule(K_KK, K, KK, k2)
+m.network_rules.add_reaction_rule(r2)
+r3 = create_unbinding_reaction_rule(K_KK, Kp, KKi, k3)
+m.network_rules.add_reaction_rule(r3)
 
-r4 = createBindingReactionRule( Kp, KK, Kp_KK, k4 )
-m.network_rules.add_reaction_rule( r4 )
-r5 = createUnbindingReactionRule( Kp_KK, Kp, KK, k5 )
-m.network_rules.add_reaction_rule( r5 )
-r6 = createUnbindingReactionRule( Kp_KK, Kpp, KKi, k6 )
-m.network_rules.add_reaction_rule( r6 )
-
-
-r7 = createBindingReactionRule( Kpp, P, Kpp_P, k1 )
-m.network_rules.add_reaction_rule( r7 )
-r8 = createUnbindingReactionRule( Kpp_P, Kpp, P, k2 )
-m.network_rules.add_reaction_rule( r8 )
-r9 = createUnbindingReactionRule( Kpp_P, Kp, Pi, k3 )
-m.network_rules.add_reaction_rule( r9 )
-
-r10 = createBindingReactionRule( Kp, P, Kp_P, k4 )
-m.network_rules.add_reaction_rule( r10 )
-r11 = createUnbindingReactionRule( Kp_P, Kp, P, k5 )
-m.network_rules.add_reaction_rule( r11 )
-r12 = createUnbindingReactionRule( Kp_P, K, Pi, k6 )
-m.network_rules.add_reaction_rule( r12 )
+r4 = create_binding_reaction_rule(Kp, KK, Kp_KK, k4)
+m.network_rules.add_reaction_rule(r4)
+r5 = create_unbinding_reaction_rule(Kp_KK, Kp, KK, k5)
+m.network_rules.add_reaction_rule(r5)
+r6 = create_unbinding_reaction_rule(Kp_KK, Kpp, KKi, k6)
+m.network_rules.add_reaction_rule(r6)
 
 
-r13 = createUnimolecularReactionRule( KKi, KK, ki )
-m.network_rules.add_reaction_rule( r13 )
-r14 = createUnimolecularReactionRule( Pi, P, ki )
-m.network_rules.add_reaction_rule( r14 )
+r7 = create_binding_reaction_rule(Kpp, P, Kpp_P, k1)
+m.network_rules.add_reaction_rule(r7)
+r8 = create_unbinding_reaction_rule(Kpp_P, Kpp, P, k2)
+m.network_rules.add_reaction_rule(r8)
+r9 = create_unbinding_reaction_rule(Kpp_P, Kp, Pi, k3)
+m.network_rules.add_reaction_rule(r9)
 
-s.setModel(m)
+r10 = create_binding_reaction_rule(Kp, P, Kp_P, k4)
+m.network_rules.add_reaction_rule(r10)
+r11 = create_unbinding_reaction_rule(Kp_P, Kp, P, k5)
+m.network_rules.add_reaction_rule(r11)
+r12 = create_unbinding_reaction_rule(Kp_P, K, Pi, k6)
+m.network_rules.add_reaction_rule(r12)
 
 
-vtklogger = VTKLogger(s, dataDirectory, bufferSize=None, no_shells=no_shells, 
-                      extraParticleStep=False, color_dict=color_dict)
+r13 = create_unimolecular_reaction_rule(KKi, KK, ki)
+m.network_rules.add_reaction_rule(r13)
+r14 = create_unimolecular_reaction_rule(Pi, P, ki)
+m.network_rules.add_reaction_rule(r14)
+
+
+vtklogger = vtklogger.VTKLogger(s, dataDirectory, buffer_size=None,     
+                                show_shells=show_shells, 
+                                extra_particle_step=False,
+                                color_dict=color_dict)
 
 
 if LOGGER == True:
     logname = model
-    l = Logger( s, logname = logname )
+    l = Logger(s, logname = logname)
 
-    rfile = open( 'data/' + logname + '_reactions.dat', 'w' )
+    rfile = open('data/' + logname + '_reactions.dat', 'w')
 
 
-    #l.setParticleOutput( ('Ea','X','EaX','Xp','Xpp','EaI') )
-    l.setParticleOutInterval( 1e-0 )
+    #l.setParticleOutput(('Ea','X','EaX','Xp','Xpp','EaI'))
+    l.setParticleOutInterval(1e-0)
     l.log()
 
 
@@ -226,8 +240,8 @@ while currentTime < endTime:
     if LOGGER == True:
         if s.lastReaction:
             r = s.lastReaction
-            line = '( %18.18g,\t%s,\t%s )\n' % ( s.t, r.reactants, r.products )
-            rfile.write( line )
+            line = '(%18.18g,\t%s,\t%s)\n' % (s.t, r.reactants, r.products)
+            rfile.write(line)
             rfile.flush()
 
             l.log()
